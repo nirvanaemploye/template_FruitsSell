d90704f685d6c89ba8ff0a7a2cf4accec9a3294d
Dhruvkumar-Mistry <dhruvkumarmistry82@gmail.com>
2025-06-05 16:28:29 +0530

    footer response

--- src/Share/Footer/Footer.jsx
+++ src/Share/Footer/Footer.jsx
@@ -22,28 +22,28 @@ const bgStyle = {
 const Footer = () => {
   return (
     <footer style={bgStyle} >
-      <div data-aos="fade" className=" container py-24 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-14">
+      <div data-aos="fade" className="container py-10 md:py-24 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-7 md:gap-y-14">
         {/* Contact Info */}
-        <div className="flex flex-col gap-10">
-          <h1 className="text-secondary text-[40px] flex flex-col">
+        <div className="flex flex-col gap-5 md:gap-10">
+          <h1 className="text-secondary text-[32px] md:text-[40px] flex flex-col">
             Contact Info{" "}
-            <span className="bg-white h-1 w-1/5 rounded-full"></span>
+            <span className="bg-white h-0.5 md:h-1 w-16 md:w-1/5 rounded-full"></span>
           </h1>
-          <div className="flex flex-col gap-6 text-white text-xl font-medium">
-            <p className="flex gap-2 text-white">
-              <span className="text-secondary text-2xl">
+          <div className="flex flex-col gap-3 md:gap-6 text-white text-xl font-medium">
+            <p className="flex text-lg md:text-xl gap-2 text-white">
+              <span className="text-secondary text-xl md:text-2xl">
                 <FaLocationDot />
               </span>
               JL Raya, Glanyar, Bali - 80571
             </p>
-            <p className="flex gap-2 text-white">
-              <span className="text-secondary text-3xl">
+            <p className="flex text-lg md:text-xl gap-2 text-white">
+              <span className="text-secondary text-2xl md:text-3xl">
                 <MdCall />
               </span>
               +651 432 89044
             </p>
-            <p className="flex gap-2 text-white">
-              <span className="text-secondary text-3xl">
+            <p className="flex text-lg md:text-xl gap-2 text-white">
+              <span className="text-secondary text-2xl md:text-3xl">
                 <MdEmail />
               </span>
               fruitstore@domain.com
@@ -51,18 +51,18 @@ const Footer = () => {
           </div>
         </div>
         {/* Quick Links */}
-        <div className="flex flex-col gap-10 lg:ms-9">
-          <div className="flex flex-col justify-center gap-8">
-            <h1 className="text-secondary text-[40px] flex flex-col">
+        <div className="flex flex-col gap-5 md:gap-10 lg:ms-9">
+          <div className="flex flex-col justify-center gap-4 md:gap-8">
+            <h1 className="text-secondary text-[32px] md:text-[40px] flex flex-col">
               Quick Links
-              <span className="bg-white h-1 w-1/5 rounded-full"></span>
+              <span className="bg-white h-0.5 md:h-1 w-16 md:w-1/5 rounded-full"></span>
             </h1>
             <div>
-              <ul className="flex flex-col gap-4">
+              <ul className="flex flex-col gap-2 md:gap-4">
                 {navItems.map((item) => (
                   <li
                     key={item.name}
-                    className="text-white text-xl "
+                    className="text-white text-lg md:text-xl "
                   >
                     <Link to={item.link} className="hover:text-secondary duration-300 transform">{item.name}</Link>
                   </li>
@@ -72,10 +72,10 @@ const Footer = () => {
           </div>
         </div>
         {/* Connect With Us */}
-        <div className="flex flex-col gap-10">
-          <h1 className="text-secondary text-[40px] flex flex-col">
+        <div className="flex flex-col gap-5 md:gap-10">
+          <h1 className="text-secondary text-[32px] md:text-[40px] flex flex-col">
             Connect With Us{" "}
-            <span className="bg-white h-1 w-1/5 rounded-full"></span>
+            <span className="bg-white h-0.5 md:h-1 w-16 md:w-1/5 rounded-full"></span>
           </h1>
           <p className="text-white ">
             Lorem ipsum dolor sit amet, consectetur adipisc pulvinar ligula
@@ -83,15 +83,15 @@ const Footer = () => {
             malesuada consequat.
           </p>
           <div className="text-white flex gap-5">
-            <FaFacebookF className="bg-primary rounded-full p-2 text-4xl hover:bg-secondary duration-300 " />
-            <FaTwitter className="bg-primary rounded-full p-2 text-4xl hover:bg-secondary duration-300" />
-            <FaGoogle className="bg-primary rounded-full p-2 text-4xl hover:bg-secondary duration-300" />
-            <FaWhatsapp className="bg-primary rounded-full p-2 text-4xl hover:bg-secondary duration-300" />
+            <FaFacebookF className="bg-primary rounded-full p-1.5 md:p-2 text-3xl md:text-4xl hover:bg-secondary duration-300 " />
+            <FaTwitter className="bg-primary rounded-full p-1.5 md:p-2 text-3xl md:text-4xl hover:bg-secondary duration-300" />
+            <FaGoogle className="bg-primary rounded-full p-1.5 md:p-2 text-3xl md:text-4xl hover:bg-secondary duration-300" />
+            <FaWhatsapp className="bg-primary rounded-full p-1.5 md:p-2 text-3xl md:text-4xl hover:bg-secondary duration-300" />
           </div>
         </div>
       </div>
       <hr className="borde border-gray-600" />
-      <p className="py-5 text-center text-base text-gray-400">Copyright © 2023 Fruit Store</p>
+      <p className=" py-3 md:py-5 text-center text-sm md:text-base text-gray-400">Copyright © 2023 Fruit Store</p>
     </footer>
   );
 };
