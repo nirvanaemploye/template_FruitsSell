a85ce7d065997f790a77599e8fdf09c9e3085092
Dhruvkumar-Mistry <dhruvkumarmistry82@gmail.com>
2025-06-05 15:31:52 +0530

    Update Choose.jsx

--- src/Components/AboutPage/Why/Choose.jsx
+++ src/Components/AboutPage/Why/Choose.jsx
@@ -36,7 +36,7 @@ const data = [
 const Choose = () => {
   return (
     <section style={bgStyle} className="py-12 md:py-24 overflow-hidden">
-      <div className="container space-x-5 md:space-y-12">
+      <div className="container space-y-5 md:space-y-12">
         <Title>Why Choose Us</Title>
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-10 p-6 sm:p-4">
           {data.map((item) => {
