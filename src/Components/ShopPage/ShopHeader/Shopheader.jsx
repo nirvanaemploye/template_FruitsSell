20947347308235c5eba2b686ab769a5d578d330e
Dhruvkumar-Mistry <dhruvkumarmistry82@gmail.com>
2025-06-05 15:35:02 +0530

    k

--- src/Components/ShopPage/ShopHeader/Shopheader.jsx
+++ src/Components/ShopPage/ShopHeader/Shopheader.jsx
@@ -6,12 +6,12 @@ import { motion } from 'motion/react';
 
 const Shopheader = () => {
   return (
-    <section  className="py-24">
+    <section  className="py-12 mdpy-24">
       <div className=" overfclow-x-hidden container relative grid grid-cols-1 lg:grid-cols-2 gap-10 lg:gap-0">
         {/* text content */}
         <div data-aos="fade-right" className="flex flex-col justify-center items-center lg:items-start gap-8 text-center lg:text-left">
           <h1 className="text-7xl sm:text-8xl 2xl:text-9xl">Shop <span className="text-secondary">Now</span></h1>
-          <p className="sm:w-2/3 sm:leading-8">
+          <p className="w-2/3 sm:leading-8">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua.
           </p>
@@ -26,7 +26,7 @@ const Shopheader = () => {
         <div className="flex justify-center items-end w-full ">
             <img
              data-aos="zoom-in"
-             
+             loading='eager'
              src={HeadImg} alt="About Us Img" className="lg:scale-125 lg:me-32"/>
         </div>
       </div>
